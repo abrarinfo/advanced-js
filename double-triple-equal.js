067674a8c8fd97bbf08017b603e3bc41bc703629
Abrar903 <princeofosun0003@gmail.com>
2021-02-06 08:20:49 +0600

    map filter

---
+++ double-triple-equal.js
@@ -0,0 +1,10 @@
+// in js double equal(==) is use for check the value
+// in js triple equal(===) is use for check the value and type
+
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
---
+++ map-filter.js
@@ -0,0 +1,29 @@
+const arr = [3, 5, 7, 8, 11];
+// const output = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+// const result = function square(element) {
+//     return element * element;
+// }
+
+// arr.map(square())
+
+// const result = arr.map(function(arr) {
+//     return arr * arr;
+// })
+
+// const square = arr => arr * arr;
+// const square = x => x * x;
+
+// const result = arr.map(x => x * x);
+
+// const bigger = arr.filter(x => x >= 7)
+
+const isThere = arr.find(x => x > 7);
+
+console.log(isThere);
